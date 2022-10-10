2f937e7e53b516f11f5ed4bc5e994606f8db67a2
SHIO777 <shiotatsu1015@gmail.com>
2022-10-11 08:09:33 +0900

    [add] SQLSTATE[42000]の解決 after('id')を削除

--- database/migrations/2022_10_10_020252_create_todos_table.php
+++ database/migrations/2022_10_10_020252_create_todos_table.php
@@ -19,7 +19,9 @@ return new class extends Migration
             $table->text('description');
             $table->date('deadline');
             $table->integer('importance');
-            $table->foreignId('user_id')->after('id')->nullable()->constrained()->cascadeOnDelete();
+            // afterはcolumnをaddするときにのみ使用可．新規作成時は使用不可．
+            // $table->foreignId('user_id')->after('id')->nullable()->constrained()->cascadeOnDelete();\// $table->foreignId('user_id')->after('id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->timestamps();
             //$table->boolean('repeat');
             //$table->double('achievement', 3, 2);
