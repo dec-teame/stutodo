0194c0344783587ee31552994d5849e6033c66cb
SHIO777 <shiotatsu1015@gmail.com>
2022-10-11 09:32:54 +0900

    [add] TodosSeeder

--- database/seeders/TodosSeeder.php
+++ database/seeders/TodosSeeder.php
@@ -2,11 +2,15 @@
 // to add this file, run command below
 // ./vendor/bin/sail php artisan make:seeder TodosSeeder 
 
+// to generate seed data, run command below
+// ./vendor/bin/sail php artisan db:seed --class=TodosSeeder
+
 
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
 
 class TodosSeeder extends Seeder
 {
