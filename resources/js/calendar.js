3880ed251200206ee1b7996313438a00a8afade3
SHIO777 <shiotatsu1015@gmail.com>
2022-10-14 22:37:08 +0900

    [fix] エラー解決のためcalendar.jsを修正

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -2,15 +2,20 @@ import '@fullcalendar/core/vdom'; // for Vite
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
-var calendarEl = document.getElementById("calendar");
+var calendar;
+let calendarEl = document.getElementById("calendar");
 
-let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
-    initialView: "dayGridMonth",
-    headerToolbar: {
-        left: "prev,next today",
-        center: "title",
-        right: "",
-    },
-});
-calendar.render();
+// Cannot read property '__k' of nullエラーを避けるため
+// 下のif文を追加。calendarElがnullでなけらばif文の中実行。
+if (calendarEl != null) {
+    let calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin],
+        initialView: "dayGridMonth",
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: "",
+        },
+    });
+    calendar.render();
+}
